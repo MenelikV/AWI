0ab629cf2411cc2f7c44bda634d4bf5e9e816c16
mvero-ext <mvero-ext@assystem.com>
2019-01-07 17:27:40 +0100

    changed MCI config

--- AWI/api/services/MCIConfig.js
+++ AWI/api/services/MCIConfig.js
@@ -4,70 +4,70 @@ module.exports = {
     format: "000000",
     // TODO Check time with moment js
     time: {
-      "minute": -1
+      "minutes": -1
     },
   },
   CG: {
     id: "88U02301--",
     format: "00.00",
     time: {
-      "minute": -1
+      "minutes": -1
     },
   },
   ZCG: {
     id: "88U02370--",
     format: "0.00",
     time: {
-      "minute": +1
+      "minutes": +1
     },
   },
   ZFW: {
     id: "88U02357--",
     format: "000000",
     time: {
-      "minute": -1
+      "minutes": -1
     }
   },
   ZFCG: {
     id: "88U02358--",
     format: "00.00",
     time: {
-      "minute": -1
+      "minutes": -1
     }
   },
   MYZFW: {
     id: "88U02359--",
     format: "0000",
     time: {
-      "minute": -1
+      "minutes": -1
     }
   },
   ZZFW: {
     id: "88U02392--",
     format: "0.00",
     time: {
-      "minute": +1
+      "minutes": +1
     }
   },
   LP: {
     id: "88U02302--",
     format: "000",
     time: {
-      "minute": -1
+      "minutes": -1
     }
   },
   ST_INIT: {
     id: "88U02302--",
     time: "0",
     format: {
-      "minute": -1
+      "minutes": -1
     }
   },
   CREW: {
     id: "88U02344--",
     format: "000000",
     time: {
-      "minute": -1
+      "minutes": -1
     }
   },
   getParameterOnEnd: function(){
