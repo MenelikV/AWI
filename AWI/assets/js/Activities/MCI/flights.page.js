47f351b654c8319c3491eae915ed9bdd47502004
mvero-ext <mvero-ext@assystem.com>
2019-01-07 17:27:26 +0100

    bug fix on MCI table

--- AWI/assets/js/Activities/MCI/flights.page.js
+++ AWI/assets/js/Activities/MCI/flights.page.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   $('#aircrafts-mci').DataTable();
   $('.dataTables_length').addClass('bs-select');
 
-  $("#aircrafts tbody tr").on("click", function (event) {
+  $("#aircrafts-mci tbody tr").on("click", function (event) {
     var currentRow = $(this).closest("tr");
     var aircraft = currentRow.find("td").eq(1).text().replace(/\s/g, '');
     var msn = currentRow.find("td").eq(2).text().replace(/\s/g, '');
