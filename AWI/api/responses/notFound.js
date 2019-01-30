9de276a0b3f9d1dff8714c8ad8a4740317f839d7
mvero-ext <mvero-ext@assystem.com>
2019-01-30 16:30:47 +0100

    same page for 500 ans 404 status

--- AWI/api/responses/notFound.js
+++ AWI/api/responses/notFound.js
@@ -1,6 +1,4 @@
 module.exports = function notFound(data, options){
-    options = options || {}
-    var activity = options.activity || "DGPS"
     this.res.status(404);
-    return this.res.view("404", {me: this.req.me, activity: activity, error: data})
+    return this.res.view("404", {me: this.req.me, activity: '', error: data})
 }
